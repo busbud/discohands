d9255bf06daafa33e53bfd6b90612d3791cf5736
Curtis McEnroe <programble@gmail.com>
2014-08-19 18:02:12 -0400

    Unify topic JavaScript

--- public/js/topics.js
+++ public/js/topics.js
@@ -40,4 +40,18 @@ $(function() {
 
     $this.blur();
   });
+
+  $('#submit').click(function(e) {
+    var $title = $('#title');
+    var $description = $('#description');
+
+    if (!$title.val() || $title.val().length > 255) {
+      $title.parents('.form-group').addClass('has-error');
+      e.preventDefault();
+    }
+    if ($description.val().length > 1024) {
+      $description.parents('.form-group').addClass('has-error');
+      e.preventDefault();
+    }
+  });
 });
